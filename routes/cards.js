a7ae252e353781f75d5b244735908892cbfe2b11
tony-developer <anton.serebriackoff@yandex.ru>
2020-11-28 14:17:15 +0300

    добавлен readme и скорректированы ошибки чтения файла

--- routes/cards.js
+++ routes/cards.js
@@ -1,14 +1,15 @@
 const cardRouter = require('express').Router();
 const fs = require('fs').promises;
+const path = require('path');
 
 cardRouter.get('/', (req, res) => {
-  fs.readFile('./data/cards.json', 'utf-8')
+  fs.readFile(path.join(__dirname, '..', 'data', 'cards.json'), 'utf-8')
     .then((data) => {
       const dataJson = JSON.parse(data);
       res.status(200).json(dataJson);
     })
     .catch(() => {
-      res.status(404).json({ message: 'Ошибка при чтении файла' });
+      res.status(500).json({ message: 'Ошибка при чтении файла' });
     });
 });
 
