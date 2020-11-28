c0678e81d6e229d66414f3f4f3d36ff9a6aa932c
tony-developer <anton.serebriackoff@yandex.ru>
2020-11-28 14:17:15 +0300

    добавлен readme и скорректированы ошибки чтения файла

--- routes/usersId.js
+++ routes/usersId.js
@@ -1,19 +1,21 @@
 const userIdRouter = require('express').Router();
 const fs = require('fs').promises;
+const path = require('path');
 
 userIdRouter.get('/:id', (req, res) => {
   const { id } = req.params;
-  fs.readFile('./data/users.json', 'utf-8')
+  fs.readFile(path.join(__dirname, '..', 'data', 'users.json'), 'utf-8')
     .then((data) => {
       const users = JSON.parse(data);
-      try {
-        const usersId = users.find((item) => item._id === req.params.id);
-        if (!usersId) {
-          res.status(404).json({ message: 'Нет пользователя с таким id' });
-        }
-        res.status(200).json(usersId);
-      } catch {
+      // eslint-disable-next-line no-underscore-dangle
+      const usersId = users.find((item) => item._id === id);
+      if (!usersId) {
+        res.status(404).json({ message: 'Нет пользователя с таким id' });
       }
+      res.status(200).json(usersId);
+    })
+    .catch(() => {
+      res.status(500).send({ message: 'Ошибка при чтении файла' });
     });
 });
 
